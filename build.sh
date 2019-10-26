090e2b215004ceed3331c56ac801e4d1f7852c37
ubuntu18.04-zmao <15866605371@163.com>
2019-10-26 08:23:41 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-printf "\t=========== Building eosio.contracts ===========\n\n"
+printf "\t=========== Building dimension.contracts ===========\n\n"
 
 RED='\033[0;31m'
 NC='\033[0m'
